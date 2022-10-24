bc84a01bd2b0faeffaf6379fa107e1dc82b20687
Ole <ol02@gmx.net>
2022-10-24 11:28:29 +0200

    init

---
+++ agents/game_utils.py
@@ -0,0 +1,82 @@
+from enum import Enum
+import numpy as np
+
+BoardPiece = np.int8  # The data type (dtype) of the board
+NO_PLAYER = BoardPiece(0)  # board[i, j] == NO_PLAYER where the position is empty
+PLAYER1 = BoardPiece(1)  # board[i, j] == PLAYER1 where player 1 (player to move first) has a piece
+PLAYER2 = BoardPiece(2)  # board[i, j] == PLAYER2 where player 2 (player to move second) has a piece
+
+BoardPiecePrint = str  # dtype for string representation of BoardPiece
+NO_PLAYER_PRINT = BoardPiecePrint(' ')
+PLAYER1_PRINT = BoardPiecePrint('X')
+PLAYER2_PRINT = BoardPiecePrint('O')
+
+PlayerAction = np.int8  # The column to be played
+
+
+class GameState(Enum):
+    IS_WIN = 1
+    IS_DRAW = -1
+    STILL_PLAYING = 0
+
+
+def initialize_game_state() -> np.ndarray:
+    """
+    Returns an ndarray, shape (6, 7) and data type (dtype) BoardPiece, initialized to 0 (NO_PLAYER).
+    """
+    raise NotImplementedError
+
+
+def pretty_print_board(board: np.ndarray) -> str:
+    """
+    Should return `board` converted to a human readable string representation,
+    to be used when playing or printing diagnostics to the console (stdout). The piece in
+    board[0, 0] should appear in the lower-left. Here's an example output, note that we use
+    PLAYER1_Print to represent PLAYER1 and PLAYER2_Print to represent PLAYER2):
+    |==============|
+    |              |
+    |              |
+    |    X X       |
+    |    O X X     |
+    |  O X O O     |
+    |  O O X X     |
+    |==============|
+    |0 1 2 3 4 5 6 |
+    """
+    raise NotImplementedError()
+
+
+def string_to_board(pp_board: str) -> np.ndarray:
+    """
+    Takes the output of pretty_print_board and turns it back into an ndarray.
+    This is quite useful for debugging, when the agent crashed and you have the last
+    board state as a string.
+    """
+    raise NotImplementedError()
+
+
+def apply_player_action(board: np.ndarray, action: PlayerAction, player: BoardPiece) -> np.ndarray:
+    """
+    Sets board[i, action] = player, where i is the lowest open row. Raises a ValueError
+    if action is not a legal move. If it is a legal move, the modified version of the
+    board is returned and the original board should remain unchanged (i.e., either set
+    back or copied beforehand).
+    """
+    raise NotImplementedError()
+
+
+def connected_four(board: np.ndarray, player: BoardPiece) -> bool:
+    """
+    Returns True if there are four adjacent pieces equal to `player` arranged
+    in either a horizontal, vertical, or diagonal line. Returns False otherwise.
+    """
+    raise NotImplementedError()
+
+
+def check_end_state(board: np.ndarray, player: BoardPiece) -> GameState:
+    """
+    Returns the current game state for the current `player`, i.e. has their last
+    action won (GameState.IS_WIN) or drawn (GameState.IS_DRAW) the game,
+    or is play still on-going (GameState.STILL_PLAYING)?
+    """
+    raise NotImplementedError()
