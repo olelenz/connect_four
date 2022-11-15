7ddb748cb7df15fb12697a2a626d81945586dbb3
Ole <ol02@gmx.net>
2022-11-15 16:13:21 +0100

    documentation and tests

--- agents/agent_minimax/minimax.py
+++ agents/agent_minimax/minimax.py
@@ -1,14 +1,33 @@
 import numpy as np
 from typing import Tuple, Optional
-
 from scipy import signal
-
-from agents.game_utils import BoardPiece, PlayerAction, NO_PLAYER, apply_player_action, PLAYER1, PLAYER2, \
-    initialize_game_state, connected_four, get_possible_moves
+from agents.game_utils import BoardPiece, PlayerAction, apply_player_action, PLAYER1, PLAYER2, initialize_game_state, connected_four, get_possible_moves
 from agents.saved_state import SavedState
 
 
 def generate_move_minimax(board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState], depth: int = 4) -> Tuple[PlayerAction, Optional[SavedState]]:
+    """
+    Generates the next move using the minimax algorithm.
+
+    Parameters
+    ----------
+    board: np.ndarray
+        Board to start with.
+
+    player: BoardPiece
+        The next player to make a move.
+
+    saved_state: Optional[SavedState]
+        Can be used to save a state of the game for future calculations. Not used here.
+
+    depth: int
+        Depth of the search tree to stop calculating.
+
+    Returns
+    -------
+    :Tuple[PlayerAction, Optional[SavedState]]
+        Tuple containing the move to play and the saved state.
+    """
     if player == PLAYER1:
         evaluation = minimax_rec(0, depth, board, player, True)
     else:
@@ -19,6 +38,32 @@ def generate_move_minimax(board: np.ndarray, player: BoardPiece, saved_state: Op
 
 def minimax_rec(current_depth: int, desired_depth: int, current_board: np.ndarray, player: BoardPiece,
                 maximize: bool) -> [(int, PlayerAction)]:
+    """
+    Recursive helper function for generate_move_minimax. Implements the minimax algorithm.
+    
+    Parameters
+    ----------
+    current_depth: int
+        The current depth in the search tree.
+
+    desired_depth: int
+        The depth in the search tree to stop calculating.
+
+    current_board: numpy.ndarray
+        The current board of the game.
+
+    player: BoardPiece
+        The next player to make a move.
+
+    maximize: bool
+        True if the current player is maximizing, false if he is minimizing.
+
+    Returns
+    -------
+    :[(int, PlayerAction)]
+        Array containing Tuples of the evaluation after playing the move, which is also returned in this Tuple.
+    """
+    # TODO: check return signature
     evaluations: [(int, PlayerAction)] = []
     possible_moves: [int] = get_possible_moves(current_board)
 
@@ -42,13 +87,30 @@ def minimax_rec(current_depth: int, desired_depth: int, current_board: np.ndarra
 
 
 def evaluate_position(board: np.ndarray) -> int:
-    output: int = 0
-    if connected_four(board, PLAYER1): return 1_000_000_000_000
-    if connected_four(board, PLAYER2): return -1_000_000_000_000
-    evaluation_board = initialize_game_state()
-    evaluation_board[board == PLAYER1] = 1
-    evaluation_board[board == PLAYER2] = -1
+    """
+    Evaluates a board position. Use convolution to assess position (two pieces together are good, one piece near to
+    an empty space is good, one piece next to a piece from the opponent is assessed as equal
 
-    for snd in [[[1, 1]], [[1], [1]], np.identity(2), np.identity(2)[::-1, ::]]:
+    Parameters
+    ----------
+    board: numpy.ndarray
+        Board to be evaluated.
+
+    Returns
+    -------
+    :int
+        The evaluation of the position.
+    """
+    if connected_four(board, PLAYER1):  # PLAYER1 won
+        return 1_000_000_000_000
+    if connected_four(board, PLAYER2):  # PLAYER2 won
+        return -1_000_000_000_000
+
+    evaluation_board = initialize_game_state()
+    evaluation_board[board == PLAYER1] = 1  # set 1 for PLAYER1
+    evaluation_board[board == PLAYER2] = -1  # set -1 for PLAYER2
+
+    output: int = 0  # initial position evaluated as equal
+    for snd in [[[1, 1]], [[1], [1]], np.identity(2), np.identity(2)[::-1, ::]]:  # setup of kernels
         output += int(sum(sum(signal.convolve2d(evaluation_board, snd, mode="valid"))))
     return output
--- agents/agent_random/random.py
+++ agents/agent_random/random.py
@@ -9,6 +9,24 @@ from agents.saved_state import SavedState
 def generate_move_random(
         board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState]
 ) -> Tuple[PlayerAction, Optional[SavedState]]:
-    # Choose a valid, non-full column randomly and return it as `action`
-    action: int = rd.choices(get_possible_moves(board))
-    return PlayerAction(action[0]), saved_state
+    """
+    Choose a valid, non-full column randomly and return it as `action`
+
+    Parameters
+    ----------
+    board: numpy.ndarray
+        Board to generate the move from.
+
+    player: BoardPiece
+        Player to make the move.
+
+    saved_state: Optional[SavedState]
+        Can be used to save a state of the game for future calculations. Not used here.
+
+    Returns
+    -------
+    :Tuple[PlayerAction, Optional[SavedState]]
+        Tuple containing the move to play and the saved state.
+
+    """
+    return PlayerAction(rd.choices(get_possible_moves(board))[0]), saved_state
